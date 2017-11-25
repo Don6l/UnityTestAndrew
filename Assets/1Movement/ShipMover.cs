a8eb5fb60a6dc415e54c336422780f34109d8362
Don6l <donalmcclean@gmail.com>
2017-11-25 19:02:01 +0000

    took too long to do initial commit

---
+++ Assets/1Movement/ShipMover.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMover : MonoBehaviour {
+	[SerializeField]private GameObject _cube;
+	[SerializeField]private float _speed = 3.0f;
+	private Vector3 _startPos;
+	private Vector3 _endPos;
+    [SerializeField] private GameObject _cylinder;
+    private float _cylinderMeshRadius;
+    private Vector3 _cylinderCenterPosition;
+
+    private void Update () {
+		Vector3 dir = (_endPos - _cube.transform.position).normalized;
+        Debug.DrawRay(_cylinderCenterPosition, Vector3.right * _cylinderMeshRadius);
+        Vector3 steeringVector;
+        Vector3 forwardVector = dir;
+        var coliderPosition = _cube.transform.position + (forwardVector*2) ;
+        Debug.DrawRay(_cube.transform.position, coliderPosition);
+        // The main goal here is to avoid the red cylinder in the middle of the scene!
+        // DO NOT use Unity's physics or navigation meshes.
+
+       
+        Vector3 vectorBetweenShipAndCylinderCenter = _cube.transform.position - _cylinderCenterPosition;
+       
+
+
+        if (((_cylinderCenterPosition)-(coliderPosition)).magnitude <= _cylinderMeshRadius)
+        {
+           
+            if (Vector3.Dot(vectorBetweenShipAndCylinderCenter.normalized, forwardVector.normalized) <0){
+                steeringVector = Vector3.Cross(dir, Vector3.up);
+            }
+            else{
+                steeringVector = Vector3.Cross(Vector3.up, dir);
+            }
+            forwardVector += steeringVector;
+            Debug.DrawRay(_cube.transform.position, steeringVector*100);
+        }
+        Debug.DrawRay(_cube.transform.position, ((_cylinderCenterPosition) - (coliderPosition * 50))*100);
+        var perpendicular = Vector3.Cross(Vector3.up, forwardVector);
+        _cube.transform.rotation = Quaternion.LookRotation(perpendicular);
+        _cube.transform.position = _cube.transform.position + forwardVector * Time.deltaTime * _speed;
+        // if cube is close to end point, reset
+        if (Vector3.Distance(_endPos, _cube.transform.position) <= (dir * Time.deltaTime * _speed).magnitude) {
+			ResetCube();
+		}
+	}
+
+	private void ResetCube() {
+		_startPos = new Vector3(4.5f, 0.0f, Random.Range(-4.5f, 4.5f));
+		_endPos = _startPos;
+		_endPos.x *= -1.0f;
+		_endPos.z = Random.Range(-4.5f, 4.5f);
+		_cube.transform.position = _startPos;
+	}
+
+	private void Start () {
+        _cylinderMeshRadius = _cylinder.GetComponent<MeshFilter>().mesh.bounds.extents.x*5;
+        
+        _cylinderCenterPosition = _cylinder.transform.position;
+        
+        ResetCube();
+    }
+}
---
+++ Assets/3Optimize/Cannon.cs
@@ -0,0 +1,149 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon : MonoBehaviour {
+	public AnimationCurve Height;
+	public AnimationCurve Distance;
+	float startTime;
+	ArrayList timeList = new ArrayList();
+	ArrayList list = new ArrayList();
+	void SpawnBall() {
+
+		GameObject go = new GameObject("Ball");
+		go.transform.parent = gameObject.transform;
+		go.transform.localPosition = Vector3.zero;
+		go.transform.localRotation = Quaternion.identity;
+		go.AddComponent<MeshRenderer>().material = GetComponent<MeshRenderer>().material;
+		list.Add(go);
+		timeList.Add(Time.time);
+
+		startTime = Time.time;
+		MeshFilter filter = go.GetComponent< MeshFilter >();
+		if (filter == null) {
+			filter = go.AddComponent< MeshFilter >();
+		}
+		Mesh mesh = filter.mesh;
+		 
+		float radius = 0.1f;
+		// Longitude |||
+		int nbLong = 24;
+		// Latitude ---
+		int nbLat = 16;
+		for (int j = 0; j < 10; ++j) {
+		#region Vertices
+		Vector3[] vertices = new Vector3[(nbLong+1) * nbLat + 2];
+		float _pi = Mathf.PI;
+		float _2pi = _pi * 2f;
+		 
+		vertices[0] = Vector3.up * radius;
+		for( int lat = 0; lat < nbLat; lat++ )
+		{
+			float a1 = _pi * (float)(lat+1) / (nbLat+1);
+			float sin1 = Mathf.Sin(a1);
+			float cos1 = Mathf.Cos(a1);
+		 
+			for( int lon = 0; lon <= nbLong; lon++ )
+			{
+				float a2 = _2pi * (float)(lon == nbLong ? 0 : lon) / nbLong;
+				float sin2 = Mathf.Sin(a2);
+				float cos2 = Mathf.Cos(a2);
+		 
+				vertices[ lon + lat * (nbLong + 1) + 1] = new Vector3( sin1 * cos2, cos1, sin1 * sin2 ) * radius;
+			}
+		}
+		vertices[vertices.Length-1] = Vector3.up * -radius;
+		#endregion
+		 
+		#region Normales		
+		Vector3[] normales = new Vector3[vertices.Length];
+		for( int n = 0; n < vertices.Length; n++ )
+			normales[n] = vertices[n].normalized;
+		#endregion
+		 
+		#region UVs
+		Vector2[] uvs = new Vector2[vertices.Length];
+		uvs[0] = Vector2.up;
+		uvs[uvs.Length-1] = Vector2.zero;
+		for( int lat = 0; lat < nbLat; lat++ )
+			for( int lon = 0; lon <= nbLong; lon++ )
+				uvs[lon + lat * (nbLong + 1) + 1] = new Vector2( (float)lon / nbLong, 1f - (float)(lat+1) / (nbLat+1) );
+		#endregion
+		 
+		#region Triangles
+		int nbFaces = vertices.Length;
+		int nbTriangles = nbFaces * 2;
+		int nbIndexes = nbTriangles * 3;
+		int[] triangles = new int[ nbIndexes ];
+		 
+		//Top Cap
+		int i = 0;
+		for( int lon = 0; lon < nbLong; lon++ )
+		{
+			triangles[i++] = lon+2;
+			triangles[i++] = lon+1;
+			triangles[i++] = 0;
+		}
+		 
+		//Middle
+		for( int lat = 0; lat < nbLat - 1; lat++ )
+		{
+			for( int lon = 0; lon < nbLong; lon++ )
+			{
+				int current = lon + lat * (nbLong + 1) + 1;
+				int next = current + nbLong + 1;
+		 
+				triangles[i++] = current;
+				triangles[i++] = current + 1;
+				triangles[i++] = next + 1;
+		 
+				triangles[i++] = current;
+				triangles[i++] = next + 1;
+				triangles[i++] = next;
+			}
+		}
+		 
+		//Bottom Cap
+		for( int lon = 0; lon < nbLong; lon++ )
+		{
+			triangles[i++] = vertices.Length - 1;
+			triangles[i++] = vertices.Length - (lon+2) - 1;
+			triangles[i++] = vertices.Length - (lon+1) - 1;
+		}
+		#endregion
+		 
+		mesh.vertices = vertices;
+		mesh.normals = normales;
+		mesh.uv = uvs;
+		mesh.triangles = triangles;
+		 
+		mesh.RecalculateBounds();
+		;
+
+
+
+		}
+	}
+
+	Vector3 startPos;
+	Vector3 startDir;
+	void Start() {
+		startPos = this.transform.position;
+		startDir = this.transform.right;
+		InvokeRepeating("SpawnBall",0.0f, 0.7f);
+	}
+
+	void Update () {
+
+		foreach(var ball in list) {
+			GameObject go = (GameObject)ball;
+			UpdateBall(go, (float)timeList[list.IndexOf(ball)]);
+		}
+	}
+
+	void UpdateBall(GameObject ball, float time) {
+		GameObject go = (GameObject)ball;
+		float lerp = Mathf.Clamp01(Mathf.InverseLerp(time, time + 2.0f, Time.time));
+		Debug.Log(lerp);
+		go.transform.position = startPos +  Vector3.up * Height.Evaluate(lerp) + startDir * Distance.Evaluate(lerp);
+	}
+}
